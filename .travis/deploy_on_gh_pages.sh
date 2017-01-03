0abcd29413c5d50d7693df49c4d19bdd49d46c0b
Petr Horáček <phoracek@redhat.com>
2017-01-03 17:12:40 +0100

    deploy api on gh pages via travis

---
+++ .travis/deploy_on_gh_pages.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+
+function _load_ssh_key {
+    openssl aes-256-cbc -K $encrypted_06e63a7169bc_key -iv $encrypted_06e63a7169bc_iv -in .travis/travis_rsa.enc -out .travis/travis_rsa -d
+    ssh-add .travis/travis_rsa
+}
+
+
+function _init_git_config {
+    git config --global user.email "ovirt-engine-api-model@travis-ci.org"
+    git config --global user.name "oVirt Engine API Model"
+}
+
+
+function _clone_gh_pages {
+    git clone https://github.com/phoracek/ovirt-engine-api-model gh-pages
+    pushd gh-pages
+    git checkout gh-pages
+    popd
+}
+
+
+function _copy_to_master {
+    mkdir gh-pages/master
+    cp target/generated-html/model.html gh-pages/master/index.html
+}
+
+
+function _copy_to_tagged {
+    description=$(git describe)
+    version=${description:0:3}
+
+    mkdir gh-pages/${version}
+    cp target/generated-html/model.html gh-pages/${version}/index.html
+}
+
+
+function _push_gh_pages {
+    commit=$(git log --format="%H" -n 1)
+    description=$(git describe)
+
+    pushd gh-pages
+    git add .
+    git commit "gh-pages ${description} ${commit}"
+    git push origin HEAD:gh-pages
+    popd
+}
+
+
+_load_ssh_key
+_init_git_config
+_clone_gh_pages
+case "$1" in
+    master)
+        _deploy_master
+        ;;
+    tagged)
+        _deploy_tagged
+        ;;
+esac
+_push_gh_pages
